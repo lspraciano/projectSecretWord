4f3f613cb6d5e0687919fc18952c37de273fd132
Lucas Praciano <luskcct@gmail.com>
2022-07-25 02:30:23 -0300

    my first commit

---
+++ src/components/StartScreen.js
@@ -0,0 +1,13 @@
+import "./StartScreen.css";
+
+const StartScreen = () => {
+    return (
+        <div className="start-screen-zone">
+            <button className="start-screen-zone__button">
+                Clique Aqui para Começar o Jogo
+            </button>
+        </div>
+    )
+};
+
+export default StartScreen
