017f29126e675ff57787e6a524da320aab251414
Lucas Praciano <luskcct@gmail.com>
2022-07-29 02:06:31 -0300

    Atualizando README.md

--- src/data/words.js
+++ src/data/words.js
@@ -1,5 +1,5 @@
 export const wordsList = {
-    Hemácias: ["Bicôncavo", "Hemoglobina", "Drepanócitos", "Esferócitos", "Codócitos", "Dacriócitos"],
+    Hemácias: ["Bicôncavo", "Hemoglobina", "Drepanócito", "Esferócito", "Codócito", "Dacriócito"],
     Leucócitos: ["Neutrófilo", "Monócito", "Defesa", "Eosinófilo", "Granulação"],
     Microscópio: ["Objetiva", "Ocular", "Prisma", "Condensador", "Platina", "Diafragma", "Revólver"],
     Tubos: ["Heparina", "EDTA"],
